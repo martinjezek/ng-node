0029e4c09291b7e9996147a36ec7abf2c331e9ee
Martin Jezek <info@martin-jezek.com>
2014-11-30 16:52:20 +0000

    feat: add a navigation controller #11

--- public/js/app.js
+++ public/js/app.js
@@ -7,16 +7,40 @@
       'myapp-home'
     ]);
 
-    app.config(['$routeProvider', function($routeProvider) {
+    app.config(['$routeProvider', '$locationProvider', function($routeProvider) {
+
+        // application router
         $routeProvider
             .when('/', {
                 templateUrl: '/partials/home.jade',
                 controller: 'HomeController',
                 controllerAs: 'home'
-            })
-            .otherwise({
-                redirectTo: '/'
             });
+
     }]);
+
+    app.directive('navigation', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/partials/navigation.jade',
+            controller: function($location) {
+
+                // array of navigation's items
+                this.items = [
+                    {
+                        name: 'Home',
+                        url: '/'
+                    }
+                ];
+
+                // check if the current item is active
+                this.isActive = function(url) {
+                    return url === $location.path();
+                };
+
+            },
+            controllerAs: 'navigation'
+        };
+    });
 
 })();
