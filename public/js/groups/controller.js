4dbf7e999ac576fb56a1ba75df5cf9e81de4feb8
Martin Jezek <info@martin-jezek.com>
2014-12-07 20:03:23 +0000

    feat: add Group modals #5

--- public/js/groups/controller.js
+++ public/js/groups/controller.js
@@ -4,18 +4,64 @@
 
     var app = angular.module('myapp-groups', []);
 
-    var GroupsController = function(config, $http) {
-        var self = this;
+    var GroupsController = function(config, $scope, $http, modal) {
 
-        this.list = [];
-        this.loading = true;
+        $scope.groups = [];
 
-        $http.get(config.api.url + '/group').success(function(res) {
-            self.list = res;
-            self.loading = false;
-        });
+        // fetch groups
+        $scope.fetchGroups = function () {
+            $scope.loading = true;
+            $http.get(config.api.url + '/group').success(function(res) {
+                $scope.groups = res;
+                $scope.loading = false;
+            });
+        };
+        $scope.fetchGroups();
+
+        // create group
+        $scope.createGroup = function () {
+            modal.open({
+                scope: $scope,
+                templateUrl: '/modal-create-group.html',
+                controller: 'CreateGroupController'
+            });
+        };
+
+        // remove group
+        $scope.removeGroup = function (group) {
+            $scope.remove = group;
+            modal.open({
+                scope: $scope,
+                templateUrl: '/modal-remove-group.html',
+                controller: 'RemoveGroupController'
+            });
+        };
+
+    };
+
+    var CreateGroupController = function (config, $scope, $http, modalInstance) {
+        $scope.group = {};
+
+        $scope.submit = function () {
+            $http.post(config.api.url + '/group', $scope.group).success(function() {
+                $scope.fetchGroups();
+                modalInstance.close();
+            });
+        };
+    };
+
+    var RemoveGroupController = function (config, $scope, $http, modalInstance) {
+        $scope.submit = function () {
+            $http.delete(config.api.url + '/group/' + $scope.remove._id).success(function() {
+                $scope.remove = null;
+                $scope.fetchGroups();
+                modalInstance.close();
+            });
+        };
     };
 
     app.controller('GroupsController', GroupsController);
+    app.controller('CreateGroupController', CreateGroupController);
+    app.controller('RemoveGroupController', RemoveGroupController);
 
 })();
